1832f1eef6c67e4be7032f96cb5ffe897060267c
DidwardFrenkel <dfz211@nyu.edu>
2015-04-26 00:18:55 -0400

    Appended readMatrix directly into main. main.c is still improvised.

--- main.c
+++ main.c
@@ -5,13 +5,56 @@
 int main(int argc, char **argv){
 
 	double** X; // input matrix, n by m
-        int* dims = readMatrix(argv[1],X); // input layer size
-	int n = dims[0]; // example size
-	int m = dims[1];
-        free(dims);
-	int k; // hidden layer size
 
-	int i,j;
+        //read the matrix from file
+        FILE *fp;
+        fp = fopen(filename,"r");
+        if (fp == NULL) {
+          printf("ERROR: unable to read file.\n");
+          return -1;
+        }
+        char* line = NULL;
+        size_t len = 0; //line length
+        int lineLen = 0; //matrix length
+        int linenum = 0; //matrix height
+
+        //two passes, first pass to determine number of lines and line length
+        // second pass to determine line length
+
+        int passed = 0;
+        while (getline(&line,&len,fp) != -1) {
+          if (passed == 0) {
+            char* elts = strtok(line," ,\t");
+            while (elts != NULL) {
+              lineLen++;
+              elts = strtok(NULL," ,\t");
+            }
+            passed = 1;
+            free(elts);
+          }
+          linenum++;
+        }
+        fclose(fp);
+
+        //open again for pass 2
+        fp = fopen(filename,"r");
+        X = malloc(sizeof(double)*lineLen*linenum);
+        int i,j;
+        for (i = 0;i<linenum;i++) X[i] = malloc(sizeof(double)*lineLen);
+        for (i = 0;i<linenum;i++) {
+          getline(&line,&len,fp);
+          char* elts = strtok(line," ,\t");
+          for (j=0;j<lineLen;j++) {
+            X[i][j] = strtod(elts,NULL);
+            elts = strtok(NULL," ,\t");
+          }
+          free(elts);
+        }
+        fclose(fp);
+	int n = lineLen; // example size
+	int m = linenum;
+
+	int k; // hidden layer size
 
 	double* y // output, n by 1
 
